e48533c28bcd55854f141450b46632309974d9c8
Allegra Anka <allegra.anka@piano.io>
2020-09-18 21:15:30 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const videoElement = document.getElementById('video');
+const buttonElement = document.getElementById('button');
+
+async function selectMediaStream() {
+    try {
+        
+    } catch (error) {
+
+    }
+}
+
+// On page load, run function that selects media stream
+selectMediaStream();
