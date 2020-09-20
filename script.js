37f494292dccc375ba50e9d296ff7758a6b0b54e
Allegra Anka <allegra.anka@piano.io>
2020-09-20 13:12:23 -0400

    media devices api implemented, project can be optimized but is complete for mvp

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const buttonElement = document.getElementById('button');
 
 async function selectMediaStream() {
     try {
-        
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
     } catch (error) {
 
     }
 }
+
+buttonElement.addEventListener('click', async () => {
+    buttonElement.disabled = true;
+    await videoElement.requestPictureInPicture();
+    buttonElement.disabled = false;
+});
 
 // On page load, run function that selects media stream
 selectMediaStream();
